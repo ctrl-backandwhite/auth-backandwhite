0bd9ba3aa31ec09d133289ba7e2536819d47d49e
back and white <01backandwhite@gmail.com>
2025-12-08 14:51:30 +0100

    Ajustes en mensaje de validacion de campos vacios

--- src/main/java/com/backandwhite/core/domain/User.java
+++ src/main/java/com/backandwhite/core/domain/User.java
@@ -1,9 +1,9 @@
 package com.backandwhite.core.domain;
 
-import lombok.*;
-
-import java.util.Objects;
-import java.util.regex.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
 
 @With
 @Data
@@ -11,25 +11,10 @@ import java.util.regex.Pattern;
 @AllArgsConstructor
 public class User {
 
-    private static final Pattern EMAIL_PATTERN = Pattern.compile("^\\S+@\\S+\\.\\S+$");
-    private static final Pattern PHONE_PATTERN = Pattern.compile("\\+?[0-9]{10,15}");
-
     private Long id;
     private String  firstName;
     private String lastName;
     private String email;
     private String phoneNumber;
     private String password;
-
-    public void validateEmail() {
-        if (Objects.isNull(email) || !email.matches(EMAIL_PATTERN.pattern())) {
-            throw new IllegalArgumentException("Invalid email address");
-        }
-    }
-
-    public void validatePhoneNumber() {
-        if (!phoneNumber.matches(PHONE_PATTERN.pattern())) {
-            throw new IllegalArgumentException("Invalid phone number");
-        }
-    }
 }
